b5a77969add6caff571e3762213a61f26bfc5745
Flugerops <bogdan07112009@gmail.com>
2024-03-14 19:42:39 +0200

    Co-authored-by: prerreslin <prerreslin@users.noreply.github.com> Co-authored-by: UkraineGoose <Nazarbizarre@users.noreply.github.com> Co-authored-by: leonidig <leonidig@users.noreply.github.com>

--- app/routes/default.py
+++ app/routes/default.py
@@ -3,9 +3,39 @@ from flask import render_template
 
 @app.route("/")
 def index():    
-    return render_template("_index.html")
+    return render_template("_index.html", title="Zmiini Novatori")
 
-@app.route("/menu")
-def menu():
 
-    return render_template("menu.html")
+@app.route("/results")
+def results():
+    max_score = 100
+    test_name = "Name"
+    students = [
+    {
+        "name": "Yaromyr",
+        "score": 79
+        
+    },
+    {
+        "name": "Max",
+        "score": 95
+    },
+    {
+        "name": "Bogdan",
+        "score": 100
+    },
+    {
+        "name": "Leonid",
+        "score": 80
+    }]
+    
+    context = {
+        "title": "Results",
+        "students": students,
+        "test_name" : test_name,
+        "max_score" : max_score
+    }
+    
+    return render_template("results.html", **context)
+    
+    
