c6d141198a18f4fd75a4aa70129965838f07c8c6
Flugerops <bogdan07112009@gmail.com>
2024-03-19 19:02:30 +0200

    new changes

--- app/routes/default.py
+++ app/routes/default.py
@@ -3,39 +3,32 @@ from flask import render_template
 
 @app.route("/")
 def index():    
-    return render_template("_index.html", title="Zmiini Novatori")
+    return render_template("menu.html", title="Zmiini Novatori")
 
 
-@app.route("/results")
-def results():
-    max_score = 100
-    test_name = "Name"
-    students = [
-    {
-        "name": "Yaromyr",
-        "score": 79
-        
-    },
-    {
-        "name": "Max",
-        "score": 95
-    },
-    {
-        "name": "Bogdan",
-        "score": 100
-    },
-    {
-        "name": "Leonid",
-        "score": 80
-    }]
+@app.route("/ourhobbies")
+def ourhobbies():
+    ourhobbies = [
+        ("Leonid:", " Football, Programming, Gym"),
+        ("Bohdan:", " Programming, Gym, Games"),
+        ("Nazar:", " Chess, Table Tennis, Programming"),
+        ("Oleksandr:", " Programming, Games, Gym"),
     
+    ]
     context = {
-        "title": "Results",
-        "students": students,
-        "test_name" : test_name,
-        "max_score" : max_score
+        "ourhobbies" : ourhobbies,
     }
+    return render_template("ourhobbies.html", **context)
+
+
+@app.route("/goodandbad")
+def goodbad():
+    goodandbad = [
+        ("Good: ", "Teamwork, Time Management, Knowledge"),
+        ("Bad: ", "404 Not Found")
     
-    return render_template("results.html", **context)
-    
-    
+    ]
+    context = {
+        "goodandbad" : goodandbad,
+    }
+    return render_template("goodandbad.html", **context)
